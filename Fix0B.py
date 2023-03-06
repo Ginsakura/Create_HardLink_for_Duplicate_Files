3069711ad68b696abea5002a481f77465d55f7ba
ginsakura <ai.0121.000.256@gmail.com>
2023-03-06 11:55:51 +0800

    提供还原0字节文件的脚本

--- Fix0B.py
+++ Fix0B.py
@@ -8,11 +8,22 @@ import time
 
 db = sql.connect('./FileData.db')
 cur = db.cursor()
-res = cur.execute('select * from sqlite_master')
+res = cur.execute('select * from sqlite_master where type=\'table\'')
+res = res.fetchall()
 for ids in res:
 	res = cur.execute(f'select * from "{ids[1]}" where FileSize=0;')
 	for idx in res:
 		print(idx)
-		os.system(f'del /F /Q "{idx[0]}"')
-		with open(idx[0],'w',encoding='utf8') as nf:
-			nf.write('')
+		try:
+			res = os.system(f'del /F /Q "{idx[0]}"')
+			if res:
+				with open('./error.log','a',encoding='utf8') as log:
+					log.write(f'{dt.datetime.now()}\t')
+					log.write(f'\tDelete File Failure:"{idx[0]}"\n')
+			else:
+				with open(idx[0],'w',encoding='utf8') as nf:
+					nf.write('')
+		except Exception as e:
+			with open('./error.log','a',encoding='utf8') as log:
+				log.write(f'{dt.datetime.now()}\t')
+				log.write(f'\t{e}\n')
